b96a6816955e5291c0605e485eb2fdc201e3bfd3
matthewmain <matthewwmain@gmail.com>
2016-01-21 10:07:01 -0500

    adds password column to user database

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160120011519) do
     t.string   "last_name"
     t.string   "screen_name"
     t.string   "email"
+    t.string   "password"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
--- test/fixtures/users.yml
+++ test/fixtures/users.yml
@@ -7,6 +7,7 @@
 #  last_name   :string
 #  screen_name :string
 #  email       :string
+#  password    :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
