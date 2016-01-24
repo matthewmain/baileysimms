b85b5b1e88164964698db14826595fadbb93a865
matthewmain <matthewwmain@gmail.com>
2016-01-24 14:51:46 -0500

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,19 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
 
 class User < ActiveRecord::Base
 	has_many :comments
 
-	validates :user_name, presence: true
+	validates :user_name, presence: true, length: { maximum: 20 }
+	validates :email, presence: true, 
+										length: { maximum: 255 },
+										format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, 
+															message: "please enter a valid email address" },
+										uniqueness: { case_sensitive: false }
 	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120011519) do
+ActiveRecord::Schema.define(version: 20160124194311) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -51,5 +51,7 @@ ActiveRecord::Schema.define(version: 20160120011519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
