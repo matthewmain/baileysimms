02102764df633ca43a6c7909436c9f042e25afa2
matthewmain <matthewwmain@gmail.com>
2016-01-24 18:56:28 -0500

    styles gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
 	def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=150&d=mm"
     image_tag(gravatar_url, alt: user.user_name, class: "gravatar")
 	end
 
