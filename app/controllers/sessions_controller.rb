a87db7aac745c8dd95b76fee245ffc2f28339ceb
matthewmain <matthewwmain@gmail.com>
2016-01-26 16:06:12 -0500

    updates sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	render 'new'
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		#log in user, redirect to user's show page
+  	else
+  		flash.now[:danger] = 'Sorry. Invalid email or password. Try again.'
+  		render 'new'
+  	end
   end
 
   def destroy
