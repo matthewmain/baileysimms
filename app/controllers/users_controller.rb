a921b7620c3e25ca371822bec3155e8e4b10a888
matthewmain <matthewwmain@gmail.com>
2016-01-28 18:54:47 -0500

    adds logout capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #...
+      log_in @user
+      flash[:success] = "Alright. Cool. You're a member of Bailey's website."
+      redirect_to @user
     else
       render 'new'
     end
