17d9e4681e783bfabb465217dabda91626aede5c
matthewmain <matthewwmain@gmail.com>
2016-01-30 09:55:52 -0500

    adds log_out for cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,38 +21,48 @@ class User < ActiveRecord::Base
 
 	attr_accessor :remember_token
 
-	before_save { self.email = email.downcase }
 	validates :user_name, presence: true, length: { maximum: 20 }
+
+	before_save { self.email = email.downcase }
 	validates :email, presence: true, 
 										length: { maximum: 255 },
 										format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, 
 															message: "please enter a valid email address" },
 										uniqueness: { case_sensitive: false }
+
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 3 }
 	
 
-	#Returns the hash digest of the given string
+
+	### Cookies ### 
+
+	#returns the hash digest of the given string
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 																									BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
-	#Returns a random token of 22 characters (A-Z, a-z, 0-9, -, _)
+	#returns a random token of 22 characters (of: A-Z, a-z, 0-9, -, _)
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
 
-	#Remembers a user in the database for use in persistent sessions
+	#remembers a user in the database for use in persistent sessions
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 
-	#Returns true if the given token matches the digest
+	#returns true if the given token matches the digest
 	def authenticated?(remember_token)
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	#forgets a user
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 
 end
