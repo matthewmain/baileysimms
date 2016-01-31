cd1aebe55a04c0085746ec341f4ba4f0fbcdc622
matthewmain <matthewwmain@gmail.com>
2016-01-31 13:11:45 -0500

    user authorization and restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
   end
@@ -47,12 +48,21 @@ class UsersController < ApplicationController
       params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
     end
 
-    # Confirms that a user is logged in
+
+    ### BEFORE FILTERS ###
+
+    #confirms that a user is logged in
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    #confirms that a user is authorized
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
+	#checks whether a given user is the current user
+	def current_user?(user)
+		user == current_user
+	end
+
 	#returns the user corresponding to the remember token cookie
 	def current_user
 		if (user_id = session[:user_id])
