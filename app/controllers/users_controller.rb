4af9e30ddf9eb5808641a0a76c3618d144b6c7f1
matthewmain <matthewwmain@gmail.com>
2016-01-31 13:33:06 -0500

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,8 @@ class UsersController < ApplicationController
     #confirms that a user is logged in
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
+        store_location
+        flash[:danger] = "You'd better log in if you're going to try to do that sort of thing."
         redirect_to login_url
       end
     end
