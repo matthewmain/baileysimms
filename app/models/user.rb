ccf7c4dd4f021f2b525db196f1080ecbae1482c9
matthewmain <matthewwmain@gmail.com>
2016-02-03 19:17:48 -0500

    sets up account activation email verification

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,10 +61,11 @@ class User < ActiveRecord::Base
 	end
 
 	#returns true if the given token matches the digest
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
-	end
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
 	#forgets a user
 	def forget
