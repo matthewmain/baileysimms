bad0984a8a44ecd566741b42aeb3fe81b3cfa021
matthewmain <matthewwmain@gmail.com>
2016-02-05 20:21:52 -0500

    udpates signup flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Thanks for signing up! Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
