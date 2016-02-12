201884e786d315a073140114d255c500cd0721fb
matthewmain <matthewwmain@gmail.com>
2016-02-12 14:46:05 -0500

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160208023140) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
@@ -62,6 +65,6 @@ ActiveRecord::Schema.define(version: 20160208023140) do
     t.datetime "reset_sent_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
