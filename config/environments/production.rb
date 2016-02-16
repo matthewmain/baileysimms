87c5e71de5e732d9acca516af41048c630adb265
matthewmain <matthewwmain@gmail.com>
2016-02-15 20:58:05 -0500

    changes heroku name to baileysimms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://b6jwx73zgqh1e.herokuapp.com'
+  host = 'https://baileysimms.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
