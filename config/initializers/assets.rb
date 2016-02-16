83427c7067c612e155a0b0ccaf5846b670d79dff
matthewmain <matthewwmain@gmail.com>
2016-02-15 22:02:43 -0500

    adds favicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 
 Rails.application.config.assets.precompile += %w( author-name-with-feather.png )
+Rails.application.config.assets.precompile += %w( favicon.png )
