2d0eb68c45e58355b2ee54ff0e416b6372e26e14
matthewmain <matthewwmain@gmail.com>
2016-02-19 14:05:31 -0500

    adds visibility columns to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208023140) do
+ActiveRecord::Schema.define(version: 20160219185947) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -44,8 +44,10 @@ ActiveRecord::Schema.define(version: 20160208023140) do
     t.string   "title"
     t.datetime "date"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.boolean  "visible_to_public", default: false
+    t.boolean  "visible_to_users",  default: false
   end
 
   create_table "users", force: :cascade do |t|
