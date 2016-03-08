a7f689020c84b4e76bf0a9e4e383f9052f7f33a3
matthewmain <matthewwmain@gmail.com>
2016-03-08 13:15:22 -0500

    adds VIP access to posts that aren't publically visible

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219185947) do
+ActiveRecord::Schema.define(version: 20160308161209) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160219185947) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.boolean  "vip",               default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
