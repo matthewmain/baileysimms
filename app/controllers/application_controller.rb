223e5f793f71d05172751470d7d677ca34697b2c
matthewmain <matthewwmain@gmail.com>
2016-03-20 14:38:44 -0400

    adds client-side Facebook login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,13 @@ class ApplicationController < ActionController::Base
         flash[:danger] = "You'd better log in if you're going to try to do that sort of thing."
         redirect_to login_url
       end
-    end				
+    end
+
+
+    #Facebook Omniauth session
+    # def fb_current_user
+    #   @fb_current_user ||= current_user if current_user[:oauth_token]
+    # end
+    # helper_method :fb_current_user					
 
 end
