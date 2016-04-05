44cbc55b3e0eca0b0817198534ce28e5dbd790c4
matthewmain <matthewwmain@gmail.com>
2016-04-05 17:30:37 -0400

    updates giveaway winner name list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -65,6 +65,7 @@ module UsersHelper
 	 	name == "samuelmax" ||
 	 	name == "Tiff" ||
 	 	name == "BbandHockey" ||
+	 	name == "PikachuBonnie" ||
 
 	 	name == "Mr_Blue" ||
 	 	name == "Mr_Pink" ? true : false;
