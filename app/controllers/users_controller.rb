c55e151418dfe7882d0d9800b5c08ce99182f340
matthewmain <matthewwmain@gmail.com>
2017-05-11 19:09:27 -0400

    minor css adjustments, compiles css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
     @top_ten_contributors_all_time = Ranking.find_by_name("top_ten_all_time").data_hash
     @top_five_contributors_this_month = Ranking.find_by_name("top_five_this_month").data_hash
     @top_five_contributors_last_month = Ranking.find_by_name("top_five_last_month").data_hash
-    @recent_comments = Comment.limit(50)
+    @recent_comments = Comment.limit(100)
   end
 
   def new
