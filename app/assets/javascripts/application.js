1ef0a850fb749ab5dc32e460d9b91cf5dff0370a
matthewmain <matthewwmain@gmail.com>
2018-02-21 23:23:52 -0500

    animated menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,34 @@
 //= require_tree .
 
 
+$(document).ready(function(){
+
+  $("#menu_icon_open").click(function(){
+  	AnimateRotate("#menu_icon_open", 0, 180, 800);
+  	AnimateRotate("#menu_icon_close", 0, 180, 800);
+    $("#menu_div").animate({left: "0px" }, 800);
+    $("#menu_icon_open").animate({"opacity": "0"}, 800);
+    $("#menu_icon_close").animate({"opacity": "1"}, 800);
+  });
+
+  $("#menu_icon_close").click(function(){
+  	AnimateRotate("#menu_icon_open", 180, 0, 625);
+  	AnimateRotate("#menu_icon_close", 180, 0, 625);
+    $("#menu_div").animate({left: "-426px" }, 800);
+    $("#menu_icon_open").animate({"opacity": "1"}, 800);
+    $("#menu_icon_close").animate({"opacity": "0"}, 625);
+  });
+
+});
+
+function AnimateRotate(element,start_degree,end_degree,duration){
+   var elem = $(element);
+   $({deg: start_degree}).animate({deg: end_degree}, {
+   duration: duration,
+   step: function(now){
+      elem.css({
+        transform: "rotate("+now+"deg)"
+      });
+    }
+  });
+}
