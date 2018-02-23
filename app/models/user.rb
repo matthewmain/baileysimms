8c7c542b3d47c1b7c18e201ee903c7333c386a05
matthewmain <matthewwmain@gmail.com>
2018-02-23 16:31:54 -0500

    adds current_post column to users db and tracks users' last-visited post by post id

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,7 @@
 #  can_access_part_13 :boolean          default(FALSE)
 #  can_access_part_14 :boolean          default(FALSE)
 #  share_record       :text
+#  current_post       :integer          default(1)
 #
 # Indexes
 #
