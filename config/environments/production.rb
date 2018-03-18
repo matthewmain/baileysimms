521537b76fd233ef00b97d165c3a71858d682dc0
matthewmain <matthewwmain@gmail.com>
2018-03-18 18:30:30 -0400

    url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://baileysimms.herokuapp.com'
+  host = 'https://deadinbed.online'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -72,7 +72,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'deadinbed.online',
     :enable_starttls_auto => true
   }
 
